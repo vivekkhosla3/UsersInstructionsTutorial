c5d58db900ed62eceba1be534b5766c05cadedc0
vivekkhosla1298 <vivekkhosla1298@hotmail.com>
2020-07-26 18:31:43 -0700

    Add project files.

---
+++ UsersInstructionsTutorial/InteractionsDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+
+namespace UsersInstructionsTutorial
+{
+
+    [TestFixture]
+     public class InteractionsDemo
+    {
+        [Test]
+        public void Example1()
+        {
+            IWebDriver driver = new FirefoxDriver();
+           
+            driver.Url = "http://www.facebook.com";
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+
+            driver.Quit();
+
+        }
+    }
+}
